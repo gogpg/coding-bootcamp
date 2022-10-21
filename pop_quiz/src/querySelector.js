b3f3fec60473aa40bcb72ac8841e63f74b9a2d89
gogpg <giedre.luko@gmail.com>
2022-10-21 21:38:00 +0300

    query selector

---
+++ pop_quiz/src/querySelector.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    ///querySelector()
+
+    //const circle = document.querySelector('.circle')  //issikeliame html elementa pagal klase, kuria nurodome skliausteliuose. jei nenurodome All, tada paims tik elementa su sita klase, nors yra ju ir daugiau.
+
+    const circle = document.querySelectorAll('.circle')  //jeigu norime visu elementu su sia klase.
+
+    //const mainCircle = document.querySelector('#main')
+    const mainCircle = document.getElementById('main')     //galima pagal id ir taip issitraukti, tada jau nereikes naudoti groteliu 
+
+    // yra ir kitu variantu pagal ka issitrukti elementus, bet sie trys yra populiariausi.
+
+    
+   
+    
+
+
+
+})
