d043831cc1122bf3936f380d95b8bc95737582e0
gogpg <giedre.luko@gmail.com>
2022-11-08 20:51:40 +0200

    fetch, GET Request

---
+++ pop_quiz/src/getPostPutDelete.js
@@ -0,0 +1,24 @@
+// https://restcountries.com/ , punkapi.com      //used resources
+/// using fetch()
+
+//to GET - GET REQUEST
+
+// fetch('')          //sintakse
+//     .then((response) => response.json())
+//     .then((data) => console.log(data))
+
+// fetch('https://restcountries.com/v2/all')   //naudojamas perkelti json failui(ikeliame nuoroda i Fetch skliaustelius) tinkle ir atspauzdinti console.log
+//     .then((response) => response.json())
+//     .then((data) => console.log(data))
+
+// fetch('https://api.punkapi.com/v2/beers')
+//     .then((response) => response.json())
+//     .then((data) => console.log(data))
+
+// fetch('https://my-burger-api.herokuapp.com/burgers')
+//     .then((response) => response.json())
+//     .then((data) => console.log(data))
+
+function getData() {
+    
+}
